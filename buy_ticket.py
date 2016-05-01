f414f11916349d2ecc5f6d05073ac990140ddd66
Andrej Staruch <s.andrej.002@gmail.com>
2016-05-01 23:26:09 +0200

    finding the train

--- buy_ticket.py
+++ buy_ticket.py
@@ -10,13 +10,21 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 
 parser = argparse.ArgumentParser(description='Automated buying webticket \
-                                 for slovak rails.')
+                                 for slovak rails. You must specify origin\
+                                 city and the destination, in exactly form,\
+                                 like it is on the website\
+                                 https://ikvc.slovakrail.sk/inet-sales-web/pages/connection/initSearch.xhtml.\n\
+                                 You must specify, leaving time from origin\
+                                 city, name of the train or his number. Only\
+                                 one is required.')
 parser.add_argument('FROM', help='From this city.')
 parser.add_argument('TO', help='To this city.')
 parser.add_argument('--time', '-t', help='Time when train is leaving \
                     origin station. Format HH:MM.')
 parser.add_argument('--name', '-n', help='Exact name of the train,\
-                    e.g. \'slovenská strela\', \'csárdás\',...')
+                    e.g. \'slovenská strela\', \'csárdás\', ...')
+parser.add_argument('--number', '-nr', help='Number of the train,\
+                    e.g. 270, 271, ...')
 
 args = parser.parse_args()
 
@@ -24,8 +32,9 @@ city_from = (args.FROM)
 city_to = (args.TO)
 train_time = (args.time)
 train_name = (args.name)
+train_number = (args.number)
 
-print(city_from, city_to, train_time, train_name)
+print(city_from, city_to, train_time, train_name, train_number)
 
 driver = webdriver.Firefox()
 driver.get(
@@ -49,3 +58,13 @@ elem_city_to = driver.find_element_by_id("searchParamForm:toInput")
 elem_city_from.send_keys(city_from)
 elem_city_to.send_keys(city_to)
 elem_city_to.send_keys(Keys.RETURN)
+
+# There is now list of trains.
+delay = 5
+driver.find_element_by_xpath(
+    "//form[@id='searchForm']/div/table/tbody/tr[8]/td[3]/a"
+).click()
+delay = 5
+driver.find_element_by_xpath(
+    "//form[@id='searchForm']/div/table/tbody/tr[8]/td[3]/div/a"
+).click()
